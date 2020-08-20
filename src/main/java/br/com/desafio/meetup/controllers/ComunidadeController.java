1e09d68147469bb27c8111605b9603af7ab402e3
jvpereiraa <jvpereiraa97@gmail.com>
2020-08-20 14:26:32 -0300

    DESENVOLVIMENTO: implmentados metodos listarComunidadeCidade e ListarEventos e tirado a validacao do grava

--- src/main/java/br/com/desafio/meetup/controllers/ComunidadeController.java
+++ src/main/java/br/com/desafio/meetup/controllers/ComunidadeController.java
@@ -1,5 +1,8 @@
 package br.com.desafio.meetup.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,36 +17,67 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.desafio.meetup.daos.ComunidadeDAO;
 import br.com.desafio.meetup.models.Comunidade;
+import br.com.desafio.meetup.models.ComunidadeJson;
+import br.com.desafio.meetup.models.Evento;
 import br.com.desafio.meetup.validation.ComunidadeValidation;
 
 @Controller
-@RequestMapping("comunidades")
+@RequestMapping("/comunidade")
 public class ComunidadeController {
 	
 	@Autowired	
 	private ComunidadeDAO comunidadeDao;
 	
-	@InitBinder
+	/*@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(new ComunidadeValidation());
 		
-	}
+	}*/
 	
 	@RequestMapping(value = "/form", method = RequestMethod.GET)
-	public ModelAndView form() {
+	public ModelAndView form(Comunidade comunidade) {
 		return new ModelAndView("/comunidade/form");
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView grava(@Valid Comunidade comunidade, BindingResult result,
+	public ModelAndView grava(Comunidade comunidade,
 			RedirectAttributes redirectAttributes) {
-		if(result.hasErrors()) {
-			return form();
-		}
 		comunidadeDao.gravar(comunidade);
 		redirectAttributes.addFlashAttribute("sucesso", "Comunidade Cadastrada com sucesso");
 		
 		return new ModelAndView("redirect:eventos");
+	}
+	
+	@RequestMapping(value = "/listarEventos", method = RequestMethod.GET)
+	public ModelAndView listarEventos(Comunidade comunidade, RedirectAttributes redirectAttributes) {
+		comunidade = comunidadeDao.getComunidadeById(comunidade);
+		if(comunidade.getNome() == null || comunidade.getNome().isEmpty()) {
+			redirectAttributes.addFlashAttribute("falha", "Nenhuma comunidade com esse ID");
+			redirectAttributes.addFlashAttribute("show", "mostra-div");
+			return new ModelAndView("redirect:/");
+		}
+		ComunidadeJson comunidadeJson = new ComunidadeJson();
+		comunidadeJson = comunidade.toComunidadeJson(comunidade);
+		ModelAndView modelAndView = new ModelAndView("/comunidade/listaEvento");
+		modelAndView.addObject("comunidade", comunidadeJson);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/listarComunidadeCidade", method = RequestMethod.GET)
+	public ModelAndView listarComunidadeCidade(Comunidade comunidade, RedirectAttributes redirectAttributes) {
+		List<Comunidade> comunidades = comunidadeDao.getComunidadeByCidade(comunidade);
+		if(comunidades == null || comunidades.isEmpty()) {
+			redirectAttributes.addFlashAttribute("falha", "Nenhuma comunidade nessa regiao");
+			redirectAttributes.addFlashAttribute("show", "mostra-div");
+			return new ModelAndView("redirect:/");
+		}
+		List<ComunidadeJson> comunidadeJson = new ArrayList<>();
+		for(Comunidade comunidadeTemp : comunidades) {
+			comunidadeJson.add(comunidadeTemp.toComunidadeJsonProximoEvento(comunidadeTemp));
+		}
+		ModelAndView modelAndView = new ModelAndView("/comunidade/listarComunidadeCidade");
+		modelAndView.addObject("comunidades", comunidadeJson);
+		return modelAndView;
 	}
 
 }
