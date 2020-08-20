16a159f77cef9fa44ae8628b02ddfaba0aa03b16
jvpereiraa <jvpereiraa97@gmail.com>
2020-08-20 14:26:57 -0300

    DESENVOLVIMENTO: desenvolvido metodo para salvar evento

--- src/main/java/br/com/desafio/meetup/controllers/EventoController.java
+++ src/main/java/br/com/desafio/meetup/controllers/EventoController.java
@@ -2,13 +2,18 @@ package br.com.desafio.meetup.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.desafio.meetup.daos.EventoDAO;
+import br.com.desafio.meetup.models.Comunidade;
 import br.com.desafio.meetup.models.Evento;
 
 @Controller
@@ -24,6 +29,10 @@ public class EventoController {
 		modelAndView.addObject("eventos",eventos);
 		
 		return modelAndView;
+	}
+	
+	public void grava(Evento evento) {
+		eventoDao.gravar(evento);
 	}
 	
 }
