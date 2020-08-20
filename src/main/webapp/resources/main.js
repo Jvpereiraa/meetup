820fff5659b57032760a35730f48bd27bbfa0349
jvpereiraa <jvpereiraa97@gmail.com>
2020-08-20 14:21:18 -0300

    DESENVOLVIMENTO: adiciona as classes main para css e js

---
+++ src/main/webapp/resources/main.js
@@ -0,0 +1,16 @@
+//var btn = document.getElementById('btn-div');
+var teste = document.getElementById('mostra-div');
+var container = document.querySelector('.teste-desafio-display');
+if(teste != null){
+	 container.style.display = 'block';
+}
+/*
+var container = document.querySelector('.teste-desafio');
+btn.addEventListener('click', function() {
+    
+  if(container.style.display === 'none') {
+      container.style.display = 'inline-block';
+  } else {
+      container.style.display = 'none';
+  }
+});*/
