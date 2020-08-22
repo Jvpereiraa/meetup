1a87d8ebe77c718600751aeae418107fec0c3a70
jvpereiraa <jvpereiraa97@gmail.com>
2020-08-22 13:43:33 -0300

    DESENVOLVIMENTO: Organizacao do codigo e adicao de profiles

--- src/main/java/br/com/desafio/meetup/conf/JPAConfiguration.java
+++ src/main/java/br/com/desafio/meetup/conf/JPAConfiguration.java
@@ -3,8 +3,10 @@ package br.com.desafio.meetup.conf;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -16,29 +18,41 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JPAConfiguration {
 	
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 		JpaVendorAdapter vendoAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setPackagesToScan("br.com.desafio.meetup.models"); //escanear para criar as entidades no banco
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setJpaProperties(additionalProperties());
+		
 		factoryBean.setJpaVendorAdapter(vendoAdapter);
 		
+	    
+	    
+	    
+	    return factoryBean;
+	}
+
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("jpa.generate-ddl", "true");
+		properties.setProperty("hibernate.show_sql", "true"); // permite ver o sql que e gerado pelo hibernate
+	    properties.setProperty("hibernate.hbm2ddl.auto", "create"); //para o hibernate gerenciar o banco
+		return properties;
+	}
+
+	@Bean
+	@Profile("dev")
+	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
 		dataSource.setUsername("root");
 		dataSource.setPassword("");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/meetup?useTimezone=true&serverTimezone=UTC");
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		
-		factoryBean.setDataSource(dataSource);
-		
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		properties.setProperty("hibernate.show_sql", "true"); // permite ver o sql que e gerado pelo hibernate
-	    properties.setProperty("hibernate.hbm2ddl.auto", "update"); //para o hibernate gerenciar o banco
-	    
-	    factoryBean.setJpaProperties(properties);
-	    
-	    factoryBean.setPackagesToScan("br.com.desafio.meetup.models"); //escanear para criar as entidades no banco
-	    
-	    return factoryBean;
+		return dataSource;
 	}
 	
 	//Metodo que vai criar/cuidar das transicoes
